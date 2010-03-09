9ff790decb1cfc38302e60da085efac373651229
Øyvind Jensen <jensen.oyvind@gmail.com>
2010-03-09 14:50:42 +0100

    Added tests for ThreeTensorMatrixElement.  Unittest: recoupling of matrix elements.

--- sympy/physics/tests/test_braket.py
+++ sympy/physics/tests/test_braket.py
@@ -46,10 +46,10 @@ def test_QuantumState():
 def test_Dagger():
     assert Dagger(FermKet('a')) == FermBra('a')
     assert Dagger(FermBra('a')) == FermKet('a')
-    assert Dagger(FermBra(FermBra('a'),FermBra('b'))) == FermKet(FermKet('a'),FermKet('b')) 
+    assert Dagger(FermBra(FermBra('a'),FermBra('b'))) == FermKet(FermKet('a'),FermKet('b'))
     assert Dagger(SphFermKet('a')) == SphFermBra('a')
     assert Dagger(SphFermBra('a')) == SphFermKet('a')
-    assert Dagger(SphFermBra('c',SphFermBra('a'),SphFermBra('b'))) == SphFermKet('c',SphFermKet('a'),SphFermKet('b')) 
+    assert Dagger(SphFermBra('c',SphFermBra('a'),SphFermBra('b'))) == SphFermKet('c',SphFermKet('a'),SphFermKet('b'))
 
 def test_antiparticle_Dagger_commutation():
     assert SphFermKet('a',hole=True) != SphFermKet('a',hole=False)
@@ -174,40 +174,99 @@ def test_ReducedMatrixElement():
     bra_a = SphFermBra('a')
     ket_b = SphFermKet('b')
     Op = SphericalTensorOperator('T','k','q')
-    assert ReducedMatrixElement(bra_a, Op, ket_b, wigner_eckardt=True)==ClebschGordanCoefficient(j_b,m_b,k,q,j_a,m_a)*ReducedMatrixElement(bra_a, Op, ket_b) 
+    assert ReducedMatrixElement(bra_a, Op, ket_b, wigner_eckardt=True)==ClebschGordanCoefficient(j_b,m_b,k,q,j_a,m_a)*ReducedMatrixElement(bra_a, Op, ket_b)
     assert ReducedMatrixElement(bra_a, Op, ket_b)._get_reduction_factor()==ClebschGordanCoefficient(j_b,m_b,k,q,j_a,m_a)
     assert ReducedMatrixElement(bra_a, Op, ket_b)._get_ThreeTensorMatrixElement()==ThreeTensorMatrixElement(bra_a, Op, ket_b)
 
     bra_ac = SphFermBra('ac', bra_a, SphFermBra('c'))
-    assert ReducedMatrixElement(bra_ac, Op, ket_b, wigner_eckardt=True)==ClebschGordanCoefficient(j_b,m_b,k,q,J_ac,M_ac)*ReducedMatrixElement(bra_ac, Op, ket_b) 
+    assert ReducedMatrixElement(bra_ac, Op, ket_b, wigner_eckardt=True)==ClebschGordanCoefficient(j_b,m_b,k,q,J_ac,M_ac)*ReducedMatrixElement(bra_ac, Op, ket_b)
     assert ReducedMatrixElement(bra_ac, Op, ket_b)._get_reduction_factor()==ClebschGordanCoefficient(j_b,m_b,k,q,J_ac,M_ac)
     assert ReducedMatrixElement(bra_ac, Op, ket_b)._get_ThreeTensorMatrixElement()==ThreeTensorMatrixElement(bra_ac, Op, ket_b)
 
 def test_DirectMatrixElement():
     Op = SphericalTensorOperator('T','k','q')
-    bra_a = SphFermBra('a')
-    bra_b = SphFermBra('b')
-    ket_c = SphFermKet('c')
-    ket_d = SphFermKet('d')
-    raises(WignerEckardDoesNotApply, 'DirectMatrixElement(bra_a, Op, ket_c).use_wigner_eckardt()')
-    assert DirectMatrixElement((bra_a,bra_b), Op, (ket_c,ket_d))==-DirectMatrixElement((bra_a,bra_b), Op, (ket_d,ket_c))
-    assert DirectMatrixElement((bra_a,bra_b), Op, (ket_c,ket_d))==-DirectMatrixElement((bra_b,bra_a), Op, (ket_c,ket_d)) 
-    assert DirectMatrixElement((bra_a,bra_b), Op, (ket_c,ket_d))==DirectMatrixElement((bra_b,bra_a), Op, (ket_d,ket_c))
-
-    assert DirectMatrixElement((bra_a,bra_b), Op, (ket_c,ket_d)).shift_vacuum([bra_b,ket_d])==DirectMatrixElement((bra_a, -Dagger(ket_d)), Op, (-Dagger(bra_b),ket_c))
-    assert DirectMatrixElement((bra_a,bra_b), Op, (ket_c,ket_d)).shift_vacuum([bra_b])==DirectMatrixElement((bra_a), Op, (-Dagger(bra_b),ket_c, ket_d))
-    assert DirectMatrixElement((bra_a,bra_b), Op, (ket_c,ket_d)).shift_vacuum([bra_a,bra_b])==DirectMatrixElement(None, Op, (-Dagger(bra_b),-Dagger(bra_a),ket_c, ket_d))
-
-def test_ThreeTensorMatrixElement():
-    Op = SphericalTensorOperator('T','k','q')
     a = SphFermBra('a')
     b = SphFermBra('b')
     c = SphFermKet('c')
     d = SphFermKet('d')
+    raises(WignerEckardDoesNotApply, 'DirectMatrixElement(a, Op, c).use_wigner_eckardt()')
+
+    DME = DirectMatrixElement
+
+    # symmetries
+    M = DME((a,b),Op,(c,d))
+    assert M ==-DME((a,b), Op, (d,c))
+    assert M ==-DME((b,a), Op, (c,d))
+    assert M == DME((b,a), Op, (d,c))
+
+    # vacuum shifting
+    assert M.shift_vacuum([b])==DME((a), Op, (c, d,-Dagger(b)))
+    assert M.shift_vacuum([d])==DME((a, b,-Dagger(d)), Op, (c))
+    assert M.shift_vacuum([b,d])==-DME((a, -Dagger(d)), Op, (c, -Dagger(b)))
+    assert M.shift_vacuum([a,b])==DME(None, Op, (c, d, -Dagger(b),-Dagger(a)))
+    assert M.shift_vacuum([c,d])==DME((a, b, -Dagger(d),-Dagger(c)), Op, None)
+    assert M.shift_vacuum([b,a,d,c])==DME((-Dagger(d),-Dagger(c)), Op, (-Dagger(b), -Dagger(a)))
+
+    assert M.shift_vacuum([b,d])==M.shift_vacuum([d,b])
+    assert M.shift_vacuum([a,b,c,d])==M.shift_vacuum([b,a,c,d])
+
+def test_ThreeTensorMatrixElement():
+    j_a, m_a = symbols('j_a m_a')
+    j_b, m_b = symbols('j_b m_b')
+    j_c, m_c = symbols('j_c m_c')
+    j_d, m_d = symbols('j_d m_d')
+    k, q = symbols('k q')
+    J_ab, M_ab = symbols('J_ab M_ab')
+    J_cd, M_cd = symbols('J_cd M_cd')
+    Op = SphericalTensorOperator('T',k,q)
+    Bra = SphFermBra
+    Ket = SphFermKet
+    a,b = Bra('a'), Bra('b')
+    c,d = Ket('c'), Ket('d')
+
+    # trivial tests
     assert ThreeTensorMatrixElement(a,Op,c) == ThreeTensorMatrixElement(a,Op,c).get_direct_product_ito_self()
     assert DirectMatrixElement(a,Op,c) == ThreeTensorMatrixElement(a,Op,c).as_direct_product()
 
+    # test coupling and decoupling
+    assert DirectMatrixElement((a,b),Op,(c,d)) == ThreeTensorMatrixElement(Bra('ab',a,b),Op,Ket('cd',c,d)).get_related_direct_matrix()
+    assert ThreeTensorMatrixElement(Bra('ab',a,b),Op,Ket('cd',c,d)).get_direct_product_ito_self() == ASigma(J_ab, J_cd, M_ab, M_cd)*ClebschGordanCoefficient(j_a, m_a, j_b, m_b,J_ab, M_ab)*ClebschGordanCoefficient(j_c, m_c, j_d, m_d,J_cd, M_cd)*ThreeTensorMatrixElement(Bra('ab',a,b),Op,Ket('cd',c,d))
+    assert ThreeTensorMatrixElement(Bra('ab',a,b),Op,Ket('cd',c,d)).as_direct_product() == ASigma(m_a, m_b, m_c, m_d)*ClebschGordanCoefficient(j_a, m_a, j_b, m_b,J_ab, M_ab)*ClebschGordanCoefficient(j_c, m_c, j_d, m_d,J_cd, M_cd)*DirectMatrixElement((a,b),Op,(c,d))
+
+    # test vacuum-shifted coupling and decoupling
+    assert DirectMatrixElement((a,-b),Op,(c,-d)) == ThreeTensorMatrixElement(Bra('ab',a,-b),Op,Ket('cd',c,-d)).get_related_direct_matrix()
+    assert ThreeTensorMatrixElement(Bra('ab',a,-b),Op,Ket('cd',c,-d)).get_direct_product_ito_self() == (-1)**(j_b-m_b)*(-1)**(j_d-m_d)*ASigma(J_ab, J_cd, M_ab, M_cd)*ClebschGordanCoefficient(j_a, m_a, j_b, -m_b,J_ab, M_ab)*ClebschGordanCoefficient(j_c, m_c, j_d, -m_d,J_cd, M_cd)*ThreeTensorMatrixElement(Bra('ab',a,-b),Op,Ket('cd',c,-d))
+    assert ThreeTensorMatrixElement(Bra('ab',a,-b),Op,Ket('cd',c,-d)).as_direct_product() == (-1)**(j_b-m_b)*(-1)**(j_d-m_d)*ASigma(m_a, m_b, m_c, m_d)*ClebschGordanCoefficient(j_a, m_a, j_b, -m_b,J_ab, M_ab)*ClebschGordanCoefficient(j_c, m_c, j_d, -m_d,J_cd, M_cd)*DirectMatrixElement((a,-b),Op,(c,-d))
+
+def test_MatrixElement_recoupling():
+    j_a, m_a = symbols('j_a m_a')
+    j_b, m_b = symbols('j_b m_b')
+    j_c, m_c = symbols('j_c m_c')
+    j_d, m_d = symbols('j_d m_d')
+    k, q = symbols('k q')
+    J_ab, M_ab = symbols('J_ab M_ab')
+    J_cd, M_cd = symbols('J_cd M_cd')
+    J_ad, M_ad = symbols('J_ad M_ad')
+    J_cb, M_cb = symbols('J_cb M_cb')
+    Op = SphericalTensorOperator('T',k,q)
+    Bra = SphFermBra
+    Ket = SphFermKet
+    a,b = Bra('a'), Bra('b')
+    c,d = Ket('c'), Ket('d')
+
+    shifted = ThreeTensorMatrixElement(Bra('ad',a,-Dagger(d)),Op,Ket('cb',c,-Dagger(b)))
+    straight = ThreeTensorMatrixElement(Bra('ab',a,b),Op,Ket('cd',c,d))
+
+    # test vacuum shifted coupling
+    assert shifted.get_related_direct_matrix() == DirectMatrixElement((a,-Dagger(d)),Op,(c,-Dagger(b)))
+    assert shifted.get_related_direct_matrix(only_particle_states=True) == -DirectMatrixElement((a,b),Op,(c,d))
+
+    # test straight coupling
+    assert straight.get_related_direct_matrix() == DirectMatrixElement((a,b),Op,(c,d))
+
+    # verify equations
+    assert shifted.as_other_coupling(straight) == -(-1)**(j_b - m_b)*(-1)**(j_d - m_d)*ASigma(J_ab, J_cd, M_ab, M_cd)*ASigma(m_a, m_b, m_c, m_d)*ClebschGordanCoefficient(j_a, m_a, j_b, m_b, J_ab, M_ab)*ClebschGordanCoefficient(j_a, m_a, j_d, -m_d, J_ad, M_ad)*ClebschGordanCoefficient(j_c, m_c, j_b, -m_b, J_cb, M_cb)*ClebschGordanCoefficient(j_c, m_c, j_d, m_d, J_cd, M_cd)*straight
+
+    assert straight.as_other_coupling(shifted) == -(-1)**(j_b - m_b)*(-1)**(j_d - m_d)*ASigma(J_ad, J_cb, M_ad, M_cb)*ASigma(m_a, m_b, m_c, m_d)*ClebschGordanCoefficient(j_a, m_a, j_b, m_b, J_ab, M_ab)*ClebschGordanCoefficient(j_a, m_a, j_d, -m_d, J_ad, M_ad)*ClebschGordanCoefficient(j_c, m_c, j_b, -m_b, J_cb, M_cb)*ClebschGordanCoefficient(j_c, m_c, j_d, m_d, J_cd, M_cd)*shifted
 
 
-
-    
