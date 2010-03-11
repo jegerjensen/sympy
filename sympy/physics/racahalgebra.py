ba5975667f142c02f114e51ea53d7340f1b0957c
Øyvind Jensen <jensen.oyvind@gmail.com>
2010-03-11 10:16:44 +0100

    Added a local cache to the racahmodule

--- sympy/physics/racahalgebra.py
+++ sympy/physics/racahalgebra.py
@@ -30,6 +30,64 @@ __all__ = [
         'remove_summation_indices',
         ]
 
+LOCAL_CACHE = []
+
+def clear_local_cache():
+    """clear cache content"""
+    for item, cache in LOCAL_CACHE:
+        if not isinstance(cache, tuple):
+            cache = (cache,)
+
+        for kv in cache:
+            kv.clear()
+
+
+def locally_cacheit(func):
+    """caching decorator.
+
+       important: the result of cached function must be *immutable*
+
+
+       Example
+       -------
+
+       @cacheit
+       def f(a,b):
+           return a+b
+
+
+       @cacheit
+       def f(a,b):
+           return [a,b] # <-- WRONG, returns mutable object
+
+
+       to force cacheit to check returned results mutability and consistency,
+       set environment variable SYMPY_USE_CACHE to 'debug'
+    """
+
+    func._cache_it_cache = func_cache_it_cache = {}
+    LOCAL_CACHE.append((func, func_cache_it_cache))
+
+    def wrapper(*args, **kw_args):
+        if kw_args:
+            keys = kw_args.keys()
+            keys.sort()
+            items = [(k+'=',kw_args[k]) for k in keys]
+            k = args + tuple(items)
+        else:
+            k = args
+        try:
+            return func_cache_it_cache[k]
+        except KeyError:
+            pass
+        func_cache_it_cache[k] = r = func(*args, **kw_args)
+        return r
+
+    wrapper.__doc__ = func.__doc__
+    wrapper.__name__ = func.__name__
+
+    return wrapper
+
 class ThreeJSymbolsNotCompatibleWithSixJSymbol(Exception):
     pass
 
