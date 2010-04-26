ab9d3db18b58870ae8d2262838ac7d16ed223bdd
Øyvind Jensen <jensen.oyvind@gmail.com>
2010-04-26 20:09:05 +0200

    Implemented the 3 nonzero diagrams of Am1pA for ground state

--- private/check_recouplings_Am1pA.py
+++ private/check_recouplings_Am1pA.py
@@ -70,13 +70,14 @@ print V
 print T
 
 
+
 print
 print "*************** <i| L | > *****************"
 print
 
-l_i = MatrixElement(Dagger(i), LA, "")
-l_i_sph = ReducedMatrixElement(Dagger(i), LA, "")
-print Eq(l_ai, l_ai_sph.get_direct_product_ito_self(tjs=0))
+l_i = MatrixElement(Dagger(i), LAm1, "")
+l_i_sph = ReducedMatrixElement(Dagger(i), LAm1, "")
+print Eq(l_i, l_i_sph.get_direct_product_ito_self(tjs=0))
 
 print
 print fcode(l_i_sph.get_direct_product_ito_self(tjs=1))
@@ -85,8 +86,8 @@ print
 print "*************** <ij| L | a> *****************"
 print
 
-l_aij = MatrixElement((Dagger(i), Dagger(j)), LA, a)
-l_aij_sph = ReducedMatrixElement(SphFermBra('ij', Dagger(i), Dagger(j)), LA, a)
+l_aij = MatrixElement((Dagger(i), Dagger(j)), LAm1, a)
+l_aij_sph = ReducedMatrixElement(SphFermBra('ij', Dagger(i), Dagger(j)), LAm1, a)
 print Eq(l_aij, l_aij_sph.get_direct_product_ito_self(tjs=0))
 
 print
@@ -117,11 +118,11 @@ print fcode(t_abij_sph.get_direct_product_ito_self(tjs=1))
 
 
 print
-print "*************** <| R | i> *****************"
+print "*************** <a| R | i> *****************"
 print
 
-r_ai = MatrixElement(a, RAm1, i)
-r_ai_sph = ReducedMatrixElement(a, RAm1, i)
+r_ai = MatrixElement(Dagger(a), RA, i)
+r_ai_sph = ReducedMatrixElement(Dagger(a), RA, i)
 print Eq(r_ai, r_ai_sph.get_direct_product_ito_self(tjs=0))
 print Eq(r_ai, r_ai_sph.get_direct_product_ito_self(tjs=1))
 
@@ -131,182 +132,69 @@ print
 print "*************** <ab| R | ij> *****************"
 print
 
-r_abij = MatrixElement((Dagger(a), Dagger(b)), RAm1, (i, j))
-r_abij_sph = ReducedMatrixElement(SphFermBra('ab', Dagger(a), Dagger(b)), RAm1, SphFermKet('ij', i, j))
+r_abij = MatrixElement((Dagger(a), Dagger(b)), RA, (i, j))
+r_abij_sph = ReducedMatrixElement(SphFermBra('ab', Dagger(a), Dagger(b)), RA, SphFermKet('ij', i, j))
 print Eq(r_abij, r_abij_sph.get_direct_product_ito_self(tjs=0))
 
 print
 print fcode(r_abij_sph.get_direct_product_ito_self(tjs=1))
 
-J_ij, J_Am1, j_b, j_i, j_j = symbols('J_ij J_Am1 j_b j_i j_j', nonnegative=True)
-M_ij, M_Am1, m_b, m_i, m_j = symbols('M_ij M_Am1 m_b m_i m_j')
-
-J_Am1 = RAm1.rank
-
-
-
-
-
-SF = Symbol('SF')
 
 """
-Ket = FermKet
-Bra = FermBra
-
-i, j, k, l = make_spherical_sp_states('i j k l')
-a, b, c, d = make_spherical_sp_states('a b c d')
-
-LA, RA = make_tensor_operators('J_A M_A J_A M_A',Q.integer)
-LAm1,RAm1 = make_tensor_operators('J_Am1 M_Am1 J_Am1 M_Am1','half_integer')
-# LA = LA.subs(Symbol('t'), Symbol('L'))
-LA = Dagger(LA.subs(Symbol('t'), Symbol('L')))
-# LA = SphericalTensorOperator('L', S.Zero, S.Zero)
-RA = RA.subs(Symbol('t'), Symbol('R'))
-LAm1 = Dagger(LAm1.subs(Symbol('t'), Symbol('L')))
-RAm1 = RAm1.subs(Symbol('t'), Symbol('R'))
-V = SphericalTensorOperator('V', S.Zero, S.Zero)
-T = SphericalTensorOperator('T', S.Zero, S.Zero)
-cre_p = SphericalTensorOperator('a','j','m')
-ann_p = Dagger(SphericalTensorOperator('a','j','m'))
+_report("*** coupled elements: ***")
+_report(Eq(l_i_sph,rewrite_coupling(l_i_sph, l_i)))
+_report(Eq(l_aij_sph,rewrite_coupling(l_aij_sph, l_aij)))
+_report(Eq(r_ai_sph,rewrite_coupling(r_ai_sph, r_ai)))
+_report(Eq(r_abij_sph,rewrite_coupling(r_abij_sph, r_abij)))
+_report(Eq(t_ai_sph,rewrite_coupling(t_ai_sph, t_ai)))
+_report(Eq(t_abij_sph,rewrite_coupling(t_abij_sph, t_abij)))
 """
-print
-print "Defined tensor operators:"
-print LA
-print RA
-print LAm1
-print RAm1
-print V
-print T
 
-print
-print "==== Recoupling L_{A} a' R_{A-1} ===="
-print
-
-# """
-j_a = Symbol('j_a', nonnegative=True)
-m_a = Symbol('m_a')
-sf_reduction = (-1)**(j_a - m_a)*ClebschGordanCoefficient('J_A', 'M_A', j_a, -m_a, 'J_Am1', 'M_Am1')*ASigma('M_A', 'm_a')
-# sf_reduction = (-1)**(j_a - m_a)*ClebschGordanCoefficient(0, 0, j_a, -m_a, 'J_Am1', 'M_Am1')*ASigma('M_A', 'm_a')
-
-print "reduction factor"
-print sf_reduction
-print fcode(refine_phases(convert_cgc2tjs(sf_reduction)))
 
 l0 = Symbol('l_0')
 r0 = Symbol('r_0')
-
-# r_i = MatrixElement("",RAm1, i)
-# r_aij = MatrixElement(Dagger(a), RAm1, (i, j))
-# r_i_sph = ReducedMatrixElement("",RAm1, i)
-# r_aij_sph = ReducedMatrixElement(Dagger(a), RAm1, SphFermKet('ij',i, j))
-
-# l_ai = MatrixElement(Dagger(i),LA,a)
-# l_ai_sph = ReducedMatrixElement(Dagger(i),LA,a)
-# l_abij = MatrixElement((Dagger(i),Dagger(j)),LA,(a,b))
-# l_abij_sph = ReducedMatrixElement(SphFermBra('ij',Dagger(i),Dagger(j), reverse=0), LA, SphFermKet('ab', a, b,reverse=0))
-
-# t_ai = MatrixElement(Dagger(a),T ,i)
-# t_ai_sph = ReducedMatrixElement(Dagger(a),T ,i)
-# t_abij = MatrixElement((Dagger(a), Dagger(b)), T,(i, j))
-# t_abij_sph = ReducedMatrixElement(SphFermBra('ab',Dagger(a), Dagger(b), reverse=0), T, SphFermKet('ij', i, j,reverse=0))
-
-_report("*** coupled elements: ***")
-_report(Eq(r_i_sph,rewrite_coupling(r_i_sph, r_i)))
-_report(Eq(r_aij_sph,rewrite_coupling(r_aij_sph, r_aij)))
-_report(Eq(l_ai_sph,rewrite_coupling(l_ai_sph, l_ai)))
-_report(Eq(l_abij_sph,rewrite_coupling(l_abij_sph, l_abij)))
-_report(Eq(t_ai_sph,rewrite_coupling(t_ai_sph, t_ai)))
-_report(Eq(t_abij_sph,rewrite_coupling(t_abij_sph, t_abij)))
-
-"""
-coupled_subs = {
-        l_ai: rewrite_coupling(l_ai, l_ai_sph),
-        r_i: rewrite_coupling(r_i, r_i_sph)
-        }
-print
-_report("recoupling diagram 1")
-expr_msc = combine_ASigmas(l_ai*r_i*ASigma('m_i') * sf_reduction)
-_report(Eq(SF, expr_msc))
-expr_sph = expr_msc.subs(coupled_subs)
-_report(Eq(SF, expr_sph))
-expr_sph = convert_cgc2tjs(expr_sph)
-_report(Eq(SF, expr_sph))
-expr_sph = evaluate_sums(expr_sph)
-_report(Eq(SF, expr_sph))
-expr_sph = apply_deltas(expr_sph)
-_report(Eq(SF, expr_sph))
-expr_sph = refine_phases(expr_sph)
-_report(Eq(SF, expr_sph))
-print fcode(expr_sph)
+SF = Symbol('SF')
 
 
 print
-_report("recoupling diagram 2")
-r_bij = r_aij.subs(Dagger(a), Dagger(b))
-r_bij_sph = r_aij_sph.subs(Dagger(a), Dagger(b))
-
-coupled_subs = {
-        l_abij: rewrite_coupling(l_abij, l_abij_sph),
-        r_bij: rewrite_coupling(r_bij, r_bij_sph)
-        }
-
-expr_msc = combine_ASigmas(S(1)/2*l_abij*r_bij*ASigma('m_b','m_i','m_j') * sf_reduction)
-_report(Eq(SF, expr_msc))
-expr_sph = expr_msc.subs(coupled_subs)
-_report(Eq(SF, expr_sph))
-expr_sph = apply_orthogonality(expr_sph, ['m_b', 'm_i', 'm_j'])
-_report(Eq(SF, expr_sph))
-expr_sph = evaluate_sums(expr_sph)
-_report(Eq(SF, expr_sph))
-expr_sph = convert_cgc2tjs(expr_sph)
-_report(Eq(SF, expr_sph))
-expr_sph = refine_phases((expr_sph))
-_report(Eq(SF, expr_sph))
-expr_sph = refine_tjs2sjs(expr_sph, verbose=1, let_pass=0)
-_report(Eq(SF, expr_sph))
-print fcode(expr_sph)
-
-
-print
-print "==== Recoupling L_{A} i' R_{A-1} ===="
+print "==== Recoupling L_{A-1} i R_{A} ===="
 print
 
 
 j_i = Symbol('j_i', nonnegative=True)
 m_i = Symbol('m_i')
-sf_reduction = 1/((-1)**(j_i - m_i)*ClebschGordanCoefficient(0, 0, j_i, -m_i, 'J_Am1', 'M_Am1').get_as_ThreeJSymbol())
-
+sf_reduction = 1/((-1)**(j_i - m_i)*ClebschGordanCoefficient(0, 0, j_i, -m_i, J_Am1, M_Am1).get_as_ThreeJSymbol())
 
 coupled_subs = {
-        r_i: rewrite_coupling(r_i, r_i_sph)
+        l_i: rewrite_coupling(l_i, l_i_sph)
         }
 print
-_report("recoupling diagram 3")
-expr_msc = combine_ASigmas(l0*r_i*sf_reduction)
+_report("recoupling diagram 1")
+expr_msc = combine_ASigmas(r0*l_i*sf_reduction)
 _report(Eq(SF, expr_msc))
 expr_sph = expr_msc.subs(coupled_subs)
 _report(Eq(SF, expr_sph))
 expr_sph = convert_cgc2tjs(expr_sph)
+_report(Eq(SF, expr_sph))
+expr_sph = apply_deltas(expr_sph, remove_all=[m_i])
 _report(Eq(SF, expr_sph))
 expr_sph = refine_phases(expr_sph, verbose=1)
 _report(Eq(SF, expr_sph))
 print fcode(expr_sph)
 
-
-
 print
-_report("recoupling diagram 4")
-l_aj = l_ai.subs(Dagger(i),Dagger(j))
-l_aj_sph = l_ai_sph.subs(Dagger(i),Dagger(j))
+_report("recoupling diagram 2")
+r_aj = r_ai.subs(i,j)
+r_aj_sph = r_ai_sph.subs(i,j)
 coupled_subs = {
-        l_aj: rewrite_coupling(l_aj, l_aj_sph),
-        r_aij: rewrite_coupling(r_aij, r_aij_sph)
+        r_aj: rewrite_coupling(r_aj, r_aj_sph),
+        l_aij: rewrite_coupling(l_aij, l_aij_sph)
         }
 j_i = Symbol('j_i', nonnegative=True)
 m_i = Symbol('m_i')
-sf_reduction = (-1)**(j_i - m_i)*ClebschGordanCoefficient('J_A', 'M_A', j_i, -m_i, 'J_Am1', 'M_Am1')*ASigma('M_A', 'm_i')
+sf_reduction = (-1)**(j_i - m_i)*ClebschGordanCoefficient(J_A, M_A, j_i, -m_i, J_Am1, M_Am1)*ASigma('M_A', 'm_i')
 print
-expr_msc = combine_ASigmas(l_aj*r_aij*ASigma('m_a','m_j') * sf_reduction)
+expr_msc = combine_ASigmas(r_aj*l_aij*ASigma('m_a','m_j') * sf_reduction)
 _report(Eq(SF, expr_msc))
 expr_sph = expr_msc.subs(coupled_subs)
 _report(Eq(SF, expr_sph))
@@ -318,24 +206,32 @@ expr_sph = refine_tjs2sjs(expr_sph, verbose=1)
 _report(Eq(SF, expr_sph))
 print fcode(expr_sph)
 
+
 print
-_report("recoupling diagram 5")
-l_aj = l_ai.subs(Dagger(i),Dagger(j))
-l_aj_sph = l_ai_sph.subs(Dagger(i),Dagger(j))
-r_j = r_i.subs(i,j)
-r_j_sph = r_i_sph.subs(i,j)
+print "==== Recoupling L_{A-1} a R_{A} ===="
+print
+
+j_a = Symbol('j_a', nonnegative=True)
+m_a = Symbol('m_a')
+sf_reduction = (-1)**(j_a - m_a)*ClebschGordanCoefficient(J_A, M_A, j_a, -m_a, J_Am1, M_Am1)*ASigma(M_A, m_a)
+
+print "reduction factor"
+print sf_reduction
+print fcode(refine_phases(convert_cgc2tjs(sf_reduction)))
+
+print
+_report("recoupling diagram 3")
 coupled_subs = {
         t_ai: rewrite_coupling(t_ai, t_ai_sph),
-        l_aj: rewrite_coupling(l_aj, l_aj_sph),
-        r_j: rewrite_coupling(r_j, r_j_sph)
+        l_i: rewrite_coupling(l_i, l_i_sph)
         }
-j_i = Symbol('j_i', nonnegative=True)
-m_i = Symbol('m_i')
-sf_reduction = (-1)**(j_i - m_i)*ClebschGordanCoefficient('J_A', 'M_A', j_i, -m_i, 'J_Am1', 'M_Am1')*ASigma('M_A', 'm_i')
+
 print
-expr_msc = combine_ASigmas(l_aj*r_j*t_ai*ASigma('m_a','m_j') * sf_reduction)
+expr_msc = combine_ASigmas(t_ai*l_i*r0*ASigma('m_i') * sf_reduction)
 _report(Eq(SF, expr_msc))
 expr_sph = expr_msc.subs(coupled_subs)
+_report(Eq(SF, expr_sph))
+expr_sph = expr_sph.subs({J_A: 0, M_A:0})
 _report(Eq(SF, expr_sph))
 expr_sph = convert_cgc2tjs(expr_sph)
 _report(Eq(SF, expr_sph))
@@ -343,11 +239,86 @@ expr_sph = evaluate_sums(expr_sph)
 _report(Eq(SF, expr_sph))
 expr_sph = apply_deltas(expr_sph)
 _report(Eq(SF, expr_sph))
-expr_sph = refine_phases(expr_sph, forbidden=[S('M_A'), S('M_Am1'), S('m_a')])
+expr_sph = refine_phases(expr_sph)
 _report(Eq(SF, expr_sph))
 print fcode(expr_sph)
 
-"""
+print
+_report("recoupling diagram 5")
+l_bij = l_aij.subs(a,b)
+l_bij_sph = l_aij_sph.subs(a,b)
+coupled_subs = {
+        t_abij: rewrite_coupling(t_abij, t_abij_sph),
+        l_bij: rewrite_coupling(l_bij, l_bij_sph),
+        }
+
+print
+expr_msc = combine_ASigmas(l_bij*r0*t_abij*ASigma('m_j','m_i','m_b') * sf_reduction)
+_report(Eq(SF, expr_msc))
+expr_sph = expr_msc.subs(coupled_subs)
+_report(Eq(SF, expr_sph))
+expr_sph = expr_sph.subs({J_A:0, M_A:0})
+_report(Eq(SF, expr_sph))
+expr_sph = convert_cgc2tjs(expr_sph)
+_report(Eq(SF, expr_sph))
+expr_sph = apply_orthogonality(expr_sph, ['m_i', 'm_j'])
+_report(Eq(SF, expr_sph))
+expr_sph = evaluate_sums(expr_sph)*ASigma('m_a')/(2*j_a +1)
+_report(Eq(SF, expr_sph))
+expr_sph = apply_orthogonality(expr_sph, ['m_a', 'm_b'])
+_report(Eq(SF, expr_sph))
+expr_sph = evaluate_sums(expr_sph)
+_report(Eq(SF, expr_sph))
+print fcode(expr_sph)
+
+
+
+coupled_subs = {
+        l_i: rewrite_coupling(l_i, l_i_sph),
+        r_ai: rewrite_coupling(r_ai, r_ai_sph)
+        }
+print
+_report("recoupling diagram 4")
+expr_msc = combine_ASigmas(l_i*r_ai*ASigma('m_i') * sf_reduction)
+_report(Eq(SF, expr_msc))
+expr_sph = expr_msc.subs(coupled_subs)
+_report(Eq(SF, expr_sph))
+expr_sph = convert_cgc2tjs(expr_sph)
+_report(Eq(SF, expr_sph))
+expr_sph = evaluate_sums(expr_sph)
+_report(Eq(SF, expr_sph))
+expr_sph = apply_orthogonality(expr_sph, [M_A, m_a])
+_report(Eq(SF, expr_sph))
+expr_sph = refine_phases(expr_sph)
+_report(Eq(SF, expr_sph))
+print fcode(expr_sph)
+
+print
+_report("recoupling diagram 7")
+l_bij = l_aij.subs(a, b)
+l_bij_sph = l_aij_sph.subs(a, b)
+
+coupled_subs = {
+        r_abij: rewrite_coupling(r_abij, r_abij_sph),
+        l_bij: rewrite_coupling(l_bij, l_bij_sph)
+        }
+
+expr_msc = combine_ASigmas(S(1)/2*r_abij*l_bij*ASigma('m_b','m_i','m_j') * sf_reduction)
+_report(Eq(SF, expr_msc))
+expr_sph = expr_msc.subs(coupled_subs)
+_report(Eq(SF, expr_sph))
+expr_sph = convert_cgc2tjs(expr_sph)
+_report(Eq(SF, expr_sph))
+expr_sph = apply_orthogonality(expr_sph, ['m_i', 'm_j'])
+_report(Eq(SF, expr_sph))
+expr_sph = evaluate_sums(expr_sph)
+_report(Eq(SF, expr_sph))
+expr_sph = refine_phases((expr_sph))
+_report(Eq(SF, expr_sph))
+expr_sph = refine_tjs2sjs(expr_sph, verbose=1, let_pass=0)
+_report(Eq(SF, expr_sph))
+print fcode(expr_sph)
+
 
 print
 _report("recoupling diagram 6")
