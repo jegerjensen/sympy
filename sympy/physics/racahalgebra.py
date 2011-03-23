e81d4d5837398411def7acc5d193505d3e5f3c08
Øyvind Jensen <jensen.oyvind@gmail.com>
2011-03-23 21:53:26 +0100

    Move racah cache into separate file

--- sympy/physics/racahalgebra.py
+++ sympy/physics/racahalgebra.py
@@ -18,6 +18,8 @@ from sympy.assumptions import (
 from sympy.assumptions.handlers import CommonHandler
 from sympy.logic.boolalg import conjuncts
 
+from sympy.physics.racah_hacks import clear_local_cache, locally_cacheit
+
 def pretty_hash(expr):
     return expr._pretty_key_()
 
@@ -40,63 +42,6 @@ __all__ = [
         'remove_summation_indices',
         ]
 
-LOCAL_CACHE = []
-
-def clear_local_cache():
-    """clear cache content"""
-    for item, cache in LOCAL_CACHE:
-        if not isinstance(cache, tuple):
-            cache = (cache,)
-
-        for kv in cache:
-            kv.clear()
-
-
-def locally_cacheit(func):
-    """caching decorator.
-
-       important: the result of cached function must be *immutable*
-
-
-       Example
-       -------
-
-       @cacheit
-       def f(a,b):
-           return a+b
-
-
-       @cacheit
-       def f(a,b):
-           return [a,b] # <-- WRONG, returns mutable object
-
-
-       to force cacheit to check returned results mutability and consistency,
-       set environment variable SYMPY_USE_CACHE to 'debug'
-    """
-
-    func._cache_it_cache = func_cache_it_cache = {}
-    LOCAL_CACHE.append((func, func_cache_it_cache))
-
-    def wrapper(*args, **kw_args):
-        if kw_args:
-            keys = kw_args.keys()
-            keys.sort()
-            items = [(k+'=',kw_args[k]) for k in keys]
-            k = args + tuple(items)
-        else:
-            k = args
-        try:
-            return func_cache_it_cache[k]
-        except KeyError:
-            pass
-        func_cache_it_cache[k] = r = func(*args, **kw_args)
-        return r
-
-    wrapper.__doc__ = func.__doc__
-    wrapper.__name__ = func.__name__
-
-    return wrapper
 
 class ThreeJSymbolsNotCompatibleWithSixJSymbol(Exception):
     pass
